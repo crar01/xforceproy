abaaf9162599252022655a2afc4b8bdaca0b926e
ariel condori <arielrcr@gmail.com>
2020-04-26 22:26:01 -0400

    Updates

---
+++ precache-manifest.b05e9a076e4603bc83215b2389ba4e49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d6167fb7518a9db0259e55ef094d49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35c1769d7a97e492ef0b",
+    "url": "/static/css/main.067ccd0e.chunk.css"
+  },
+  {
+    "revision": "a0590ae3bca538b3aa0e",
+    "url": "/static/js/2.bd3b7e68.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.bd3b7e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c1769d7a97e492ef0b",
+    "url": "/static/js/main.131ebcaf.chunk.js"
+  },
+  {
+    "revision": "0e8efc75a0ffaf91b47e",
+    "url": "/static/js/runtime-main.d6dcdc78.js"
+  },
+  {
+    "revision": "6e5d6096c02e8c18ad95d40ae5bf0089",
+    "url": "/static/media/loading.6e5d6096.gif"
+  }
+]);
